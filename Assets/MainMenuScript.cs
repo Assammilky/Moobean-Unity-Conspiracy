85c5289330c57ee347a2c110ba3e4f3625df3365
Assammilk <120110580+Assammilky@users.noreply.github.com>
2026-01-28 15:46:28 +1100

    yep
    yep

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour
+{
+    
+
+    public string play_scene;
+
+    public void Play_Button()
+    {
+
+        SceneManager.LoadScene(play_scene); 
+
+    }
+
+    public void Open_Settings(){
+
+    }
+
+    public void Quit(){
+
+    }
+
+
+}
