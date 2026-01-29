9acddf79b8e4cdc91894687651aa5798dffc52fb
Assammilk <120110580+Assammilky@users.noreply.github.com>
2026-01-30 00:54:01 +1100

    Messing with vfx + cutscene

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+    }
+}
